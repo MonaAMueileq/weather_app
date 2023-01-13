d1a5c8f0995c00c6c552503d7b0dd293df1bc4a1
MonaAMueileq <monaabumuilik@gmail.com>
2023-01-13 17:00:00 +0200

    first commit

---
+++ lib/screens/location_screen.dart
@@ -0,0 +1,128 @@
+import 'package:clime_app/screens/city_screen.dart';
+import 'package:flutter/material.dart';
+import '../services/weather.dart';
+import '../utilities/constants.dart';
+
+class LocationScreen extends StatefulWidget {
+  final locationWeather;
+  LocationScreen({this.locationWeather});
+  @override
+  _LocationScreenState createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
+  late String weatherIcon;
+  late String weatherMessage;
+  late int temprature;
+  late String city;
+  WeatherModel _weatherModel = WeatherModel();
+  @override
+  void initState() {
+    updateUI(widget.locationWeather);
+    super.initState();
+  }
+
+  void updateUI(dynamic weatherData) {
+    setState(() {
+      if (weatherData == null) {
+        temprature = 0;
+        weatherIcon = 'Error';
+        weatherMessage = 'Couldnt find a location';
+        city = '';
+        return;
+      } else {
+        var condition = weatherData['weather'][0]['id'];
+        weatherIcon = _weatherModel.getWeatherIcon(condition);
+        double temp = weatherData['main']['temp'];
+        temprature = temp.toInt();
+        weatherMessage = _weatherModel.getMessage(temprature);
+        city = weatherData['name'];
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/location_background.jpg'),
+            fit: BoxFit.cover,
+            colorFilter: ColorFilter.mode(
+                Colors.white.withOpacity(0.8), BlendMode.dstATop),
+          ),
+        ),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      setState(() async {
+                        var weatherData =
+                            await _weatherModel.getLocationWeather();
+                        updateUI(weatherData);
+                      });
+                    },
+                    child: Icon(
+                      Icons.near_me,
+                      size: 50.0,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () async {
+                      var typeName = await Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CityScreen(),
+                        ),
+                      );
+                      if (typeName != null) {
+                        var weatherData =
+                            await _weatherModel.getCityLocation(typeName);
+                        updateUI(weatherData);
+                      }
+                    },
+                    child: Icon(
+                      Icons.location_city,
+                      size: 50.0,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 15.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      '$temprature°',
+                      style: kTempTextStyle,
+                    ),
+                    Text(
+                      weatherIcon,
+                      style: kConditionTextStyle,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 15.0),
+                child: Text(
+                  ' $weatherMessage. \nIn $city!',
+                  textAlign: TextAlign.right,
+                  style: kMessageTextStyle,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
